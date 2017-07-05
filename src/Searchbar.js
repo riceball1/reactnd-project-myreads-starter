ef98b4cae08796a38dfb333ac391ee6ab2f32d18
Dana Ng <ngdana1@gmail.com>
2017-07-04 23:11:41 -0700

    updated with react-router-dom, abstracted searchbar into individual component

---
+++ src/Searchbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+class Searchbar extends React.Component {
+
+	render() {
+		return (
+			<div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                <input type="text" placeholder="Search by title or author"/>
+              </div>
+              </div>
+            </div>
+		)
+	}
+}
+
+
+export default Searchbar
